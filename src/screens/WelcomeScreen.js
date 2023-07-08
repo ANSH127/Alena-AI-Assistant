4484a13a23e0e4252314f0dfeb7b7cffbe084565
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-08 10:50:18 +0530

    project setup

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function WelcomeScreen() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>WelcomeScreen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
