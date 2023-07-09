73fb1024678c1f934702b49b1444f65bb4d6bb02
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-09 10:34:07 +0530

    fixing api call

--- src/api/openAI.js
+++ src/api/openAI.js
@@ -11,38 +11,14 @@ const chatGptEndpoint = 'https://api.openai.com/v1/chat/completions'
 const dalleEndpoint = 'https://api.openai.com/v1/images/generations'
 
 export const apiCall=async(prompt,messages)=>{
-    try {
-        const res= await client.post(chatGptEndpoint,{
-            model: "gpt-3.5-turbo",
-            messages:[{
-                role:'user',
-                content: `Does this message want to generate an AI picture, image, art or anything similar? ${prompt} . Simply answer with a yes or no.`
-
-
-            }]
-        })
-        // console.log('res',res.data.choices[0].message);
-        let isArt=res.data?.choices[0]?.message?.content;
-        if(isArt.toLowerCase().includes('yes')){
-            // generate art
-            // console.log('delle api call');
-            return dalleApiCall(prompt,messages || []);
-        }
-        else{
-            // chat
-            // console.log('chatgpt api call')
-            // console.log('prompt',prompt);
-            // console.log(messages);
-            
-            return chatgptApiCall(prompt,messages || []);
-        }
-
-
-
-        
-    } catch (error) {
-        console.log('error',error);
-        return Promise.resolve({success:false,msg:error.message})
+    prompt=prompt.toLowerCase();
+    if(prompt.includes('art') || prompt.includes('image') || prompt.includes('picture') || prompt.includes('drawing') || prompt.includes('sketch')  ){
+        // console.log('delle api call');
+        return dalleApiCall(prompt,messages || []);
+    }
+    else{
+        // console.log('chatgpt api call')
+        return chatgptApiCall(prompt,messages || []);
     }
 }
 
