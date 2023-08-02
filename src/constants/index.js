5ad391b88b06d36a599c7ddef26ccfca7acbd935
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-08-02 18:39:14 +0530

    updating file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const apiKey = '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';
+export const apiKey = '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';
 // in some cases your api key maybe already expired
 // try to use a new account to create an api key
 
